0cd8d39ad8fe0d308d61d1ecb0695f8455b5f86c
Orlando <gojer@naver.com>
2026-02-24 19:06:20 -0400

    chore: update src/llm/providers/local.ts

--- src/llm/providers/local.ts
+++
@@ -1,9 +0,0 @@
-import type { LlmProvider, LlmGenerateInput, LlmGenerateOutput } from '../types.js';
-
-export class LocalLlmProvider implements LlmProvider {
-    name = 'local';
-
-    async generate(_input: LlmGenerateInput): Promise<LlmGenerateOutput> {
-        throw new Error('LocalLlmProvider is not implemented yet');
-    }
-}
